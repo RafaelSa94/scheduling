f2b02ea4cb7ecaa43cc4e2517e82b7ac3492cb7b
Rodrigo Tavares <rodrigost23@gmail.com>
2016-07-30 12:43:58 -0400

    Protótipo inicial da interface web

---
+++ web/index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Agendamento de aulas</title>
+        <link rel="stylesheet" href="/css/bootstrap.min.css" media="screen">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <h3>Agendamento de aulas</h3>
+                <h4>Disciplinas:</h4>
+                <table class="table table-hover table-stripped table-bordered">
+                    <tr>
+                        <th>#</th>
+                        <th>Disciplina</th>
+                        <th>Professor</th>
+                        <th>Horário</th>
+                    </tr>
+                    <tr>
+                        <td>0</td>
+                        <td>Fasoejgl</td>
+                        <td>Opsrhju Alkeuf</td>
+                        <td>Seg/Qua 08:00 - 12:00</td>
+                    </tr>
+                </table>
+
+                <h4>Professores:</h4>
+                <table class="table table-hover table-stripped table-bordered">
+                    <tr>
+                        <th>#</th>
+                        <th>Professor</th>
+                        <th>Restrições</th>
+                    </tr>
+                    <tr>
+                        <td>0</td>
+                        <td>Opsrhju Alkeuf</td>
+                        <td>Sex 08:00 - 18:00</td>
+                    </tr>
+                </table>
+
+                <h4>Horário:
+                    <small><button id="" class="btn btn-default btn-xs">
+                        <span class="glyphicon glyphicon-refresh"></span>
+                        Atualizar horário
+                    </button></small>
+                </h4>
+                <table class="table table-hover table-stripped table-bordered">
+                    <tr>
+                        <th></th>
+                        <th>Segunda</th>
+                        <th>Terça</th>
+                        <th>Quarta</th>
+                        <th>Quinta</th>
+                        <th>Sexta</th>
+                    </tr>
+                    <tr>
+                        <td>8:00 - 10:00</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>10:00 - 12:00</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>14:00 - 16:00</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>16:00 - 18:00</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+        <script src="/js/bootstrap.min.js"></script>
+    </body>
+</html>
