bc190daa02cd2ff04c7b904ec7a46dda83e4bf37
Rodrigo Tavares <rodrigost23@gmail.com>
2016-08-11 15:31:26 -0400

    Permitindo indicar nome do arquivo de saída para o programa

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -2,12 +2,16 @@
 #include "./header/data.hpp"
 
 int main(int argc, char const *argv[]) {
+  if (argc < 2) {
+    std::cout << "Usage: " << argv[0] << " INPUT [OUTPUT]" << endl;
+    return -1;
+  }
   std::string input_file(argv[1]);
   Data a;
   a.load(input_file);
   // ColorGraph g(a.getVector(), a.getMap());
   ColoringAlgorithm g(ColorGraph(a.getVector(), a.getMap()), 10);
   g.run();
-  g.exportGraph("out.csv");
+  g.exportGraph(argc > 2 ? argv[2] : "out.csv");
   return 0;
 }
