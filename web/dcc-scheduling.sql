c780557fea3ac84dcc0a617b142ff348e31f2457
Rodrigo Tavares <rodrigost23@gmail.com>
2016-08-17 09:35:31 -0400

    Atualizando SQL do BD

--- web/dcc-scheduling.sql
+++ web/dcc-scheduling.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Tempo de geração: 06/08/2016 às 16:40
+-- Tempo de geração: 17/08/2016 às 09:35
 -- Versão do servidor: 5.7.13-0ubuntu0.16.04.2
 -- Versão do PHP: 7.0.8-0ubuntu0.16.04.2
 
@@ -38,7 +38,21 @@ CREATE TABLE `class` (
 --
 
 INSERT INTO `class` (`id`, `name`, `professor_id`, `semester`) VALUES
-(1, 'DCCxxx - Disciplina Algo', 1, 1);
+(16, 'DCC105', 20, 1),
+(17, 'DCC606', 22, 6),
+(18, 'DCC603', 24, 6),
+(19, 'DCC204', 27, 2),
+(20, 'DCC605', 28, 6),
+(21, 'DCC402', 27, 4),
+(22, 'DCC405', 26, 4),
+(23, 'DCC607', 28, 6),
+(24, 'DCC206', 22, 2),
+(25, 'DCC205.A', 29, 2),
+(26, 'DCC205.B', 24, 2),
+(27, 'DCC802', 24, 8),
+(28, 'DCC407', 31, 4),
+(29, 'DCC602', 30, 6),
+(30, 'DCC403', 31, 4);
 
 -- --------------------------------------------------------
 
@@ -63,7 +77,7 @@ CREATE TABLE `class_professor` (
 CREATE TABLE `professor` (
   `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
-  `constraints` set('01','02','03','04','05','06','07','08','09','10') NOT NULL
+  `constraints` set('01','02','03','04','05','06','07','08','09','10') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -71,7 +85,15 @@ CREATE TABLE `professor` (
 --
 
 INSERT INTO `professor` (`id`, `name`, `constraints`) VALUES
-(1, 'Fulano', '01,02,05,06,09');
+(20, 'George', '09,10'),
+(22, 'Herbert', '01,02,07,08,09,10'),
+(24, 'Dion', ''),
+(26, 'Thais', '01,02,03,04,05,06,07,08'),
+(27, 'Delfa', '01,02,05,06,09'),
+(28, 'Miguel', '01,02,05,06,09'),
+(29, 'Dwan', '09,10'),
+(30, 'Balico', '01,02,07,08,09,10'),
+(31, 'Lobo', '09,10');
 
 -- --------------------------------------------------------
 
@@ -80,7 +102,7 @@ INSERT INTO `professor` (`id`, `name`, `constraints`) VALUES
 --
 DROP TABLE IF EXISTS `class_professor`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `class_professor`  AS  select `class`.`id` AS `id`,`class`.`name` AS `name`,`class`.`professor_id` AS `professor_id`,`professor`.`name` AS `professor_name`,`class`.`semester` AS `semester`,`professor`.`constraints` AS `constraints` from (`professor` join `class` on((`class`.`professor_id` = `professor`.`id`))) ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `class_professor`  AS  select `class`.`id` AS `id`,`class`.`name` AS `name`,`class`.`professor_id` AS `professor_id`,`professor`.`name` AS `professor_name`,`class`.`semester` AS `semester`,`professor`.`constraints` AS `constraints` from (`class` left join `professor` on((`class`.`professor_id` = `professor`.`id`))) ;
 
 --
 -- Índices de tabelas apagadas
@@ -107,12 +129,12 @@ ALTER TABLE `professor`
 -- AUTO_INCREMENT de tabela `class`
 --
 ALTER TABLE `class`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 --
 -- AUTO_INCREMENT de tabela `professor`
 --
 ALTER TABLE `professor`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 --
 -- Restrições para dumps de tabelas
 --
